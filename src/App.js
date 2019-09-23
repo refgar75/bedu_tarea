50ff8cd45c3eee1c6c119dfbf92385299ac79fd7
Your Name <you@example.com>
2019-09-22 20:16:22 -0500

    jnjj

--- src/App.js
+++ src/App.js
@@ -4,21 +4,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="contenedor">
+        <div class="elemento elemento1">1</div>
+        <div class="elemento elemento2">2</div>
+        <div class="elemento elemento3">3</div>
+        <div class="elemento elemento4">4</div>
     </div>
   );
 }
