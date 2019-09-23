c64877799a62e22107218887d618391626463ee9
Your Name <you@example.com>
2019-09-22 20:25:32 -0500

    egundi

---
+++ src/components/comentario.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Comentario(props) {
+    return (
+        <p> {props.Comentario} </p>
+
+)
+}
+
+export default Comentario
---
+++ src/components/titulo.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Titulo(props) {
+    return (
+        <h1> {props.name} </h1>
+
+)
+}
+
+export default Titulo
